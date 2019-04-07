83a8f91bb0e82ec5859d5f0b0cd08ffac329fd06
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-04-07 04:04:39 +0200

    Install MW on TravisCI

---
+++ .travis.install.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+set -x
+
+originalDirectory=$(pwd)
+
+cd ..
+
+wget https://github.com/wikimedia/mediawiki-core/archive/$MW.tar.gz
+tar -zxf $MW.tar.gz
+mv mediawiki-$MW phase3
+
+cd phase3
+
+composer install --prefer-source
+
+if [ "$DB" == "postgres" ]
+then
+	psql -c 'create database its_a_mw;' -U postgres
+	php maintenance/install.php --dbtype $DBTYPE --dbuser postgres --dbname its_a_mw --pass nyan TravisWiki admin --scriptpath /TravisWiki
+else
+	mysql -e 'create database its_a_mw;'
+	php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin --scriptpath /TravisWiki
+fi
+
+cd vendor
+mkdir mediawiki
+cd mediawiki
+
+cp -r $originalDirectory doctrine-connection
+
+cd doctrine-connection
+composer install
+
+cd ../../..
+
+echo 'include_once( __DIR__ . "/vendor/mediawiki/doctrine-connection/vendor/autoload.php" );' >> LocalSettings.php
+
+echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
+echo 'ini_set("display_errors", 1);' >> LocalSettings.php
+echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
+echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
+echo "putenv( 'MW_INSTALL_PATH=$(pwd)' );" >> LocalSettings.php
+
+php maintenance/update.php --quick
