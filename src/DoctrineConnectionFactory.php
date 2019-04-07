23632f30ffec3fe7b2a4e8938c8008a4455f0a76
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-04-07 04:53:12 +0200

    Support sqlite

--- src/DoctrineConnectionFactory.php
+++ src/DoctrineConnectionFactory.php
@@ -3,23 +3,41 @@
 namespace MediaWiki\DoctrineConnection;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
 use MediaWiki\DoctrineConnection\PackagePrivate\MysqliDriver;
 use Wikimedia\Rdbms\Database;
 use Wikimedia\Rdbms\DatabaseMysqli;
+use Wikimedia\Rdbms\DatabaseSqlite;
 
 class DoctrineConnectionFactory {
 
-	public function connectionFromDatabase( Database $db ) {
+	public function connectionFromDatabase( Database $db ): Connection {
 		if ( $db instanceof DatabaseMysqli ) {
-			$reflectionProperty = new \ReflectionProperty( DatabaseMysqli::class, 'conn' );
-			$reflectionProperty->setAccessible( true );
-
-			return new Connection(
-				[],
-				new MysqliDriver( $reflectionProperty->getValue( $db ) )
-			);
+			return $this->newMysqliBasedConnection( $db );
 		}
 
+		if ( $db instanceof DatabaseSqlite ) {
+			return $this->newSqliteBasedConnection( $db );
+		}
+
+		throw new \RuntimeException( 'Unsupported database type' );
+	}
+
+	private function newMysqliBasedConnection( DatabaseMysqli $db ): Connection {
+		$reflectionProperty = new \ReflectionProperty( DatabaseMysqli::class, 'conn' );
+		$reflectionProperty->setAccessible( true );
+
+		return new Connection(
+			[],
+			new MysqliDriver( $reflectionProperty->getValue( $db ) )
+		);
+	}
+
+	private function newSqliteBasedConnection( DatabaseSqlite $db ): Connection {
+		$reflectionProperty = new \ReflectionProperty( DatabaseSqlite::class, 'conn' );
+		$reflectionProperty->setAccessible( true );
+
+		return DriverManager::getConnection( [ 'pdo' => $reflectionProperty->getValue( $db ) ] );
 	}
 
 }
--- tests/Unit/DoctrineConnectionFactoryTest.php
+++ tests/Unit/DoctrineConnectionFactoryTest.php
@@ -4,19 +4,43 @@ declare( strict_types = 1 );
 
 namespace MediaWiki\DoctrineConnection\Tests\Unit;
 
+use DatabaseTestHelper;
 use Doctrine\DBAL\Connection;
 use MediaWiki\DoctrineConnection\DoctrineConnectionFactory;
+use Wikimedia\Rdbms\Database;
+use Wikimedia\Rdbms\DatabaseSqlite;
 
 /**
  * @covers \MediaWiki\DoctrineConnection\DoctrineConnectionFactory
  */
 class DoctrineConnectionFactoryTest extends \MediaWikiTestCase {
 
-	public function testGetTrue() {
+	public function testMainDatabase() {
+		$this->assertCanTurnDatabaseIntoConnection( $this->db );
+	}
+
+	private function assertCanTurnDatabaseIntoConnection( Database $db ) {
+		$connection = ( new DoctrineConnectionFactory() )->connectionFromDatabase( $db );
+
 		$this->assertInstanceOf(
 			Connection::class,
-			( new DoctrineConnectionFactory() )->connectionFromDatabase( $this->db )
+			$connection
 		);
+
+		// TODO: assert functioning
+	}
+
+	public function testSqliteDatabase() {
+		$this->assertCanTurnDatabaseIntoConnection(
+			DatabaseSqlite::newStandaloneInstance( ':memory:' )
+		);
+	}
+
+	public function testGivenUnsupportedDatabase_exceptionIsThrown() {
+		$unsupportedDb = new DatabaseTestHelper( 'test' );
+
+		$this->expectException( \RuntimeException::class );
+		( new DoctrineConnectionFactory() )->connectionFromDatabase( $unsupportedDb );
 	}
 
 }
